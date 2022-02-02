82a64483f1ac8e8ec94c32f7a9aed5ee10ba542b
reza2boyce <reza2boyce@gmail.com>
2022-02-02 22:39:38 +0330

    a simple validated form with oop structure was practiced with simple conditional methods

--- index.php
+++ index.php
@@ -8,30 +8,33 @@
     <body>
         <div class="error-frame">
             <?php
-                function showInfo(){
-                    global $first_name, $last_name, $gender;
-                    echo "your name is : $first_name" . "<br>";
-                    echo "your last name is : $last_name" . "<br>";
-                    echo "your gender is : $gender" . "<br>";
+                function showMethod(){
+                    $methodName = htmlspecialchars($_SERVER['REQUEST_METHOD']);
+                    return $methodName;
+                }
+
+                function showFirstName(){
+                    $firstName = htmlspecialchars($_REQUEST['f-Name']);
+                    return $firstName;
+                }
+
+                function showLastName(){
+                    $lastName = htmlspecialchars($_REQUEST['l-Name']);
+                    return $lastName;
+                }
+
+                function showGender(){
+                    $gender = htmlspecialchars($_REQUEST['gender']);
+                    return $gender;
                 }
 
                 if($_SERVER['REQUEST_METHOD'] == "POST"){
-                    $first_name = htmlspecialchars($_REQUEST['f-Name']);
-                    $last_name = htmlspecialchars($_REQUEST['l-Name']);
-                    $gender = htmlspecialchars($_REQUEST['gender']);
-
-                    if(empty($first_name) && empty($last_name)){
-                        echo "the first name field is empty" . "<br>";
-                        echo "the last name field is empty" . "<br>";
-
-                    } else if(empty($first_name) && !empty($last_name)){
-                        echo "first name field is empty";
-                    } else if(empty($last_name) && !empty($first_name)){
-                        echo "last name field is empty";
-                    } else {
-                        showInfo();
-                    }
+                    echo showMethod() . "<br>";
+                    echo showFirstName() . "<br>";
+                    echo showLastName() . "<br>";
+                    echo showGender() . "<br>";
                 }
+                
             ?>
     
         </div>
